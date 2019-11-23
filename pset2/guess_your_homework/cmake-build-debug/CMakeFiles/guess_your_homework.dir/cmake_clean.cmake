840573860adf5aa705110a7f145eea395f953838
Zefu Wang <2635139041@qq.com>
2019-11-23 20:57:20 +0800

    Add files via upload

---
+++ pset2/guess_your_homework/cmake-build-debug/CMakeFiles/guess_your_homework.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guess_your_homework.dir/main.cpp.o"
+  "guess_your_homework.pdb"
+  "guess_your_homework"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/guess_your_homework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
